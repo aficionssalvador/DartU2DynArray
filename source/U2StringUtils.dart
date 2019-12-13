1e6bf2d552514af597bda89c9060e33f371dc2fc
Salvador R <sarigau@gmail.com>
2019-12-13 19:00:59 +0100

    inicial

---
+++ source/U2StringUtils.dart
@@ -0,0 +1,200 @@
+import 'U2DynArray.dart';
+import 'U2ListStrings.dart';
+import 'package:intl/intl.dart';
+
+//
+//  funcions per tractar string al estil universe
+//
+class U2StringUtils {
+  static String u2Lower(String cadena) {
+    if (cadena == null || cadena.length <= 0) {
+      return '';
+    }
+    List<String> c = cadena.split('');
+    int n = c.length;
+    for (var i = 0; i < n; i++) {
+      if (c[i] == U2RM) {
+        c[i] = U2AM;
+      } else if (c[i] == U2AM) {
+        c[i] = U2VM;
+      } else if (c[i] == U2VM) {
+        c[i] = U2SVM;
+      } else if (c[i] == U2SVM) {
+        c[i] = U2TM;
+      }
+    }
+    return c.join('');
+  }
+
+  static String u2Raise(String cadena) {
+    if (cadena == null || cadena.length <= 0) {
+      return '';
+    }
+    List<String> c = cadena.split('');
+    int n = c.length;
+    for (int i = 0; i < n; i++) {
+      if (c[i] == U2AM) {
+        c[i] = U2RM;
+      } else if (c[i] == U2VM) {
+        c[i] = U2AM;
+      } else if (c[i] == U2SVM) {
+        c[i] = U2VM;
+      } else if (c[i] == U2TM) {
+        c[i] = U2SVM;
+      }
+    }
+    return c.join("");
+  }
+
+  static String u2Field(String cadena, String separa, int pos, int num1) {
+    int pos2 = pos;
+    int num2 = num1;
+    if (cadena == null || cadena.length <= 0) {
+      return '';
+    }
+    if (separa == null || separa.length <= 0) {
+      return '';
+    }
+    if (pos == null || pos < 1) {
+      pos2 = 1;
+    }
+    if (num1 == null || num1 < 1) {
+      num2 = 1;
+    }
+    U2ListStrings s1 = new U2ListStrings(cadena, separa);
+    if (num2 == 1) {
+      return s1.field(pos2);
+    } else {
+      U2ListStrings s2 = new U2ListStrings('', separa);
+      for (int i = pos2; i < pos2 + num2; i++) {
+        s2.fieldStore(s1.field(i), i - pos2 + 1);
+      }
+      return s2.fieldJoin();
+    }
+  }
+
+  static String u2Substr(String cadena, int pos, int num1) {
+    int pos2 = pos;
+    int num2 = num1;
+    if (cadena == null || cadena.length <= 0) {
+      return '';
+    }
+    if (pos == null || pos < 1) {
+      pos2 = 1;
+    }
+    if (num1 == null || num1 == 0) {
+      num2 = 1;
+    }
+    if (num1 > 0) {
+      return cadena.substring(pos2 - 1, num2);
+    } else if (num1 < 0) {
+      return cadena.substring(pos2 + num2, -1 * num2);
+    }
+  }
+
+  static String u2Left(String cadena, int num1) {
+    int num2 = num1;
+    if (cadena == null || cadena.length <= 0) {
+      return '';
+    }
+    if (num1 == null || num1 <= 0) {
+      num2 = 1;
+    }
+    return cadena.substring(0, num2);
+  }
+
+  static String u2Rigth(String cadena, int num1) {
+    int num2 = num1;
+    if (cadena == null || cadena.length <= 0) {
+      return '';
+    }
+    if (num1 == null || num1 <= 0) {
+      num2 = 1;
+    }
+    return cadena.substring(cadena.length - num2, num2);
+  }
+
+//
+// Tactament data hora uniVerse
+//
+
+  static int u2Date() {
+    return ((DateTime
+        .now()
+        .millisecondsSinceEpoch ~/ 86400000) + 732);
+  }
+
+// hora solar
+  static num u2Time() {
+    return ((DateTime
+        .now()
+        .millisecondsSinceEpoch % 86400000) / 1000);
+  }
+
+  static int DateTime2u2Date(DateTime dt) {
+    if (dt == null) {
+      return null;
+    }
+    return ((dt.millisecondsSinceEpoch ~/ 86400000) + 732);
+  }
+
+// hora solar
+  num DateTime2u2Time(DateTime dt) {
+    if (dt == null) {
+      return null;
+    }
+    return ((dt.millisecondsSinceEpoch % 86400000) / 1000);
+  }
+
+  DateTime u2Date2DateTime(int num1) {
+    if (num1 == null) {
+      return null;
+    }
+    int v = (num1 - 732) * 86400000;
+    return (new DateTime.fromMillisecondsSinceEpoch(v));
+  }
+
+  DateTime u2Time2DateTime(num num1) {
+    if (num1 == null) {
+      return null;
+    }
+    int v = num1 * 1000;
+    return (new DateTime.fromMillisecondsSinceEpoch(v));
+  }
+
+  String DateTime2u2TADA(DateTime dt) {
+    if (dt == null) {
+      return null;
+    }
+    DateFormat formatter = new DateFormat('yyyyMMdd');
+    return formatter.format(dt);
+  }
+
+  String DateTime2u2HHMMSS(DateTime dt) {
+    if (dt==null) {
+      return null;
+    }
+    DateFormat formatter = new DateFormat('HHmmss');
+    return formatter.format(dt);
+  }
+
+/*
+var u2TADA2DateTime = function(sTada){
+if ((sTada===undefined)||(sTada==null)||(typeof sTada != 'string')) { return null; }
+var s = sTada.substring(0,4)+"-"+sTada.substring(4,6)+"-"+sTada.substring(6,8)
+return new Date(s)
+}
+
+var u2HHMMSS2DateTime = function(sHHMMSS){
+if ((sHHMMSS===undefined)||(sHHMMSS==null)||(typeof sHHMMSS != 'string')) { return null; }
+var s = sHHMMSS.substring(0,2)+":"+sTada.substring(2,4)+":"+sTada.substring(4,6)
+return new Date('1970-01-01T'+s)
+}
+
+var u2Index = function(cadena, subcadena){
+if ((cadena===undefined)||(cadena==null)||(typeof cadena != 'string')||(cadena=="")) { return 0; }
+if ((subcadena===undefined)||(subcadena==null)||(typeof subcadena != 'string')||(subcadena=="")) { return 0; }
+return cadena.indexOf(subcadena)+1
+}
+*/
+}
